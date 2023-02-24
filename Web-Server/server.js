d70af2ae616c3e9c45393c2f0618477954770afa
Ankita Shendge <ankitashendge15@gmail.com>
2023-02-24 07:54:39 -0800

    package.json added

--- Web-Server/server.js
+++ Web-Server/server.js
@@ -1,10 +1,31 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
+
+async function connectToMongo() {
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  const db = client.db("tools");
+  console.log("Connected to MongoDB!");
+  // Perform database operations here...
+  const collection = db.collection("featured_card");
+  const result = await collection.find().toArray();
+  console.log(result);
+  client.close();
+}
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
   console.log(__dirname);
+
+  connectToMongo();
   // Read the requested file from disk and stream it to the response
   if (req.url == "/") {
     res.writeHead(302, { location: "/index.html" });
